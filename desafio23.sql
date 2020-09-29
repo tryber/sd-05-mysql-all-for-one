1588e9b0ea5906fa7707a21ebbed00e60bb6b35e
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-09-29 19:20:59 -0300

    Arrumando os 'trem'

--- desafio23.sql
+++ desafio23.sql
@@ -1,7 +1,6 @@
 -- Atualize os dados de discount da tabela order_details para
 -- 30 cuja unit_price seja menor que 10.0000.
 
-SET SQL_SAFE_UPDATES = 0;
 UPDATE northwind.order_details
 SET discount = 30
-WHERE unit_price < 10.000;
+WHERE unit_price < 10;
