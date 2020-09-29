a8e0038b752ac9c338598f86ec2ebb3a9f70675b
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-09-29 18:27:56 -0300

    CC

--- desafio19.sql
+++ desafio19.sql
@@ -1,4 +1,3 @@
 -- Quantos pedidos foram feitos na tabela orders pelo employee_id igual
 -- a 5 ou 6, e que foram enviados através do método shipper_id igual a 2?
 -- Chame a coluna de orders_count.
-
