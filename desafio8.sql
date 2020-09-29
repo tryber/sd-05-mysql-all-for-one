2b4840a84abcdc04885de9d20892b2b8bacc8854
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-09-29 19:07:24 -0300

    desafio 8 completo

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `Trybe` (
+`id` INT(11) NOT NULL AUTO_INCREMENT,
+`A` VARCHAR(50),
+`Trybe` VARCHAR(50),
+`eh` VARCHAR(50),
+PRIMARY KEY (`id`));
+
+INSERT INTO Trybe (A, Trybe, eh)
+VALUES ( 5 + 6 , 'de', 2 + 8);
+
+SELECT * FROM Trybe;
