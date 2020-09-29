16d3964edc8704a633fb9441986bf9822a7dc0fb
Sidnei Ramos <sidneiramos02@gmail.com>
2020-09-29 18:47:03 -0300

    20 requisito

---
+++ desafio20.sql
@@ -0,0 +1,4 @@
+-- Os '15' não precisam dos zeros, já que eles são setados automaticamente
+-- Não precisa setar valores nulos
+INSERT INTO order_details (order_id, product_id, quantity, unity_price, discount, status_id, inventory_id)
+VALUES (69, 80, 15, 15, 0, 2, 129);
