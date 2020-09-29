da26ab2611cedd9ac408cabdbbc3e67bb49d1474
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-09-29 16:01:32 -0300

    Desafio 1 e 2 done. Exibindo nomes da tabela e todas as colunas da tabela

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,5 @@
+-- Exiba apenas os nomes do produtos na tabela products.
+
 use northwind;
 
 SELECT product_name FROM products;
---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+-- Exiba os dados de todas as colunas da tabela products.
+
+use northwind;
+
+SELECT * FROM products;
