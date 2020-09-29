a572a574bf62cdb2218a8c7d5427167fe198a533
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-09-29 19:40:03 -0300

    desafio 12 feito

--- tests/filteringChallenges.spec.js
+++ tests/filteringChallenges.spec.js
@@ -53,7 +53,7 @@ describe('Desafios sobre filtragem de dados', () => {
     });
   });
 
-  describe('Mostre as `submitted_date` de `purchase_orders` em que a `submitted_date` é do dia 26 de abril de 2006', () => {
+  describe.only('Mostre as `submitted_date` de `purchase_orders` em que a `submitted_date` é do dia 26 de abril de 2006', () => {
     it('Verifica o desafio12', async () => {
       const challengeQuery = readFileSync('desafio12.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult12');
