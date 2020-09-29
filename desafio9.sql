1c22aba3c1039179b892aa709f26a3ef2e63b30a
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-09-29 15:53:57 -0300

    9-19 filter challenges

--- desafio9.sql
+++ desafio9.sql
@@ -1,2 +1,4 @@
 -- II. Desafios sobre filtragem de dados
 -- 9. Mostre todos os valores de notes da tabela purchase_orders que não são nulos.
+
+SELECT notes FROM northwind.purchase_orders WHERE notes IS NOT NULL;
