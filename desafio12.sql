05e5ddcceb79214fa749d285cc1f54527039bf03
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-09-29 17:25:48 -0300

    Desafio 12. Filtrando por data

---
+++ desafio12.sql
@@ -0,0 +1,5 @@
+-- Mostre as submitted_date de purchase_orders em que a
+-- submitted_date é do dia 26 de abril de 2006.
+
+SELECT submitted_date from northwind.purchase_orders
+WHERE submitted_date LIKE "2006-04-26%";
