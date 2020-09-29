26042bf26e194a9ca893d5cad611abe77d3ebde5
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-09-29 19:01:31 -0300

    nova correção avaliador

--- desafio19.sql
+++ desafio19.sql
@@ -1,5 +1,4 @@
 -- Quantos pedidos foram feitos na tabela orders pelo employee_id igual a 5 ou 6, 
 -- e que foram enviados através do método shipper_id igual a 2? Chame a coluna de orders_count.
-SELECT * FROM orders;
 SELECT COUNT(*) FROM orders
 WHERE employee_id IN (5, 6) AND shipper_id = 2;
--- desafio20.sql
+++ desafio20.sql
@@ -2,7 +2,5 @@
 -- order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, 
 -- discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL e 
 -- inventory_id: 129. Obs.: o id deve ser incrementado automaticamente.
-SELECT * FROM northwind.order_details;
 INSERT INTO order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
 VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
-SELECT * FROM northwind.order_details;
