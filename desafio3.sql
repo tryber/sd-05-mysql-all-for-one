65d0ca43995467baaefcb073e33259293427d6a5
Preto115 <preto115@gmail.com>
2020-09-29 22:48:33 -0300

    desafio-3 implementado

---
+++ desafio3.sql
@@ -0,0 +1,3 @@
+SELECT id FROM northwind.products;
+-- estava eu tentando bolar uma maneira muito sofisticada de buscar a primary key, 
+-- quando na verdade era so escrever diretamente o nome da coluna que continha a PK segundo o PR de todos os colegas..
