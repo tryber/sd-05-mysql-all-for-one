27fd9d40175d69d43201d7d25deab13fdaa76649
Marylange <marylangess@gmail.com>
2020-10-07 16:51:04 -0300

    corrigindo erro 7 E 8

--- desafio7.sql
+++ desafio7.sql
@@ -1,3 +1,3 @@
 -- Mostre apenas os ids dos 5 últimos registros da tabela products (a ordernação deve ser baseada na coluna id).
 SELECT * FROM northwind.products
-ORDER BY id DESC LIMIT 5;
+ORDER BY id ASC LIMIT 40, 45;
