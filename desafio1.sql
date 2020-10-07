d4d1d45d5ab977d9fd7b25e65a1c0600bea48f88
Marylange <marylangess@gmail.com>
2020-10-07 15:09:12 -0300

    Verificando requisitos

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 -- Exiba apenas os nomes do produtos na tabela products.
 use northwind;
-SELECT product_name FROM northwind.products;
+SELECT product_name FROM products;
---
+++ desafio4.sql
@@ -0,0 +1,2 @@
+-- Conte quantos registros existem em product_name de products.
+use northwind;
