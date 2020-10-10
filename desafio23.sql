5d43f1bb0a1311ef41bf6ef088d55e2de8fff6d1
Marylange <marylangess@gmail.com>
2020-10-10 16:54:46 -0300

    requisito 23

--- desafio23.sql
+++ desafio23.sql
@@ -1,6 +1,4 @@
 -- Atualize os dados de discount da tabela order_details para 30 cuja unit_price seja menor que 10.0000.
 UPDATE order_details
 SET discount = 30
-WHERE unit_price = 10.0000;
-
-SELECT * FROM northwind.order_details;
+WHERE unit_price > 10.0000;
