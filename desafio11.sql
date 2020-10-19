389ea6d4da6766ec26af788dcc15de68e2861a1d
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-10-19 09:47:14 -0300

    corrigido erro, projeto 100%!

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,2 @@
---Testando se passa no requisito retirando o northwind pois teste está com problemas
-SELECT notes FROM purchase_orders
-WHERE notes
-LIKE "%#3_";
+SELECT notes FROM northwind.purchase_orders
+WHERE notes LIKE '%3_';
