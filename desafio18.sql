8b0e25b63c3a834faab51baefd4e613ee8d09099
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-11-29 12:06:49 -0300

    17 n 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,2 +1,3 @@
 # 18. Mostre todos os registros de `purchase_orders` que tem o
 # `supplier_id` igual a 3 e `status_id` igual a 2.
+SELECT supplier_id FROM northwind.purchase_orders WHERE supplier_id LIKE 3 AND 2;
