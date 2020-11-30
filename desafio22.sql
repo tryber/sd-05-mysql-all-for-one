6b430ae183dc65793e8a4b5a22309a6d27137ed5
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-11-30 16:05:31 -0300

    desafio 20

--- desafio22.sql
+++ desafio22.sql
@@ -1,1 +1,3 @@
-22. Atualize os dados de `discount` do `order_details` para 15.
+# 22. Atualize os dados de `discount` do `order_details` para 15.
+UPDATE northwind.order_details
+SET discount = 15;
